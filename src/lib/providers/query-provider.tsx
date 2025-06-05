9a16d810e98a0c46db93b30c6a1a7c57f9a9e143
Max aka Mosheh <moshehwebservices@live.com>
2025-06-05 16:48:25 +0300

    Update Query Provider for React Query v5

--- src/lib/providers/query-provider.tsx
+++ src/lib/providers/query-provider.tsx
@@ -5,7 +5,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 60 * 1000, // 1 minute
-      cacheTime: 5 * 60 * 1000, // 5 minutes
+      gcTime: 5 * 60 * 1000, // 5 minutes
       retry: 1,
       refetchOnWindowFocus: false,
       refetchOnReconnect: false,
